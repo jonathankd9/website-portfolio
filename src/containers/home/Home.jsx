0ca503aff7a1377292463ec2f7b44eccc8dd86c1
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-06-14 08:09:48 +0000

    update

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 	return (
 		<div>
 			{/* navbar */}
-			<div className="bg-[#F8F8F9]">
+			<div className="">
 				<div className="container flex justify-between bg-[#F8F8F9] items-center py-5">
 					<div className="text-3xl font-bold text-black">
 						Jonathan Kofi Donkor
