43e7ad11582017339144e67bab72c2882061791b
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-10-09 12:12:04 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Home from "./containers/Home";
 import {Routes, Route} from "react-router-dom";
 import Nav from "./nav/Nav";
 import FrontendSites from "./containers/FrontendSites";
-import Projects from "./containers/Projects"
+import Projects from "./containers/Projects";
 
 const App = () => {
 	return (
-		<>
+		<React.Fragment>
 			<Nav />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/frontend" element={<FrontendSites />} />
 				<Route path="/projects" element={<Projects />} />
 			</Routes>
-		</>
+		</React.Fragment>
 	);
 };
 
